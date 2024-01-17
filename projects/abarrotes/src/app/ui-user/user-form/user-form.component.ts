9cf5381b45fb30261a0124bb89cba4eab4a70bc2
linocsar <linocsar.dev@gmail.com>
2024-01-17 07:40:51 -0500

    user form terminado

--- projects/abarrotes/src/app/ui-user/user-form/user-form.component.ts
+++ projects/abarrotes/src/app/ui-user/user-form/user-form.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, Optional, inject } from '@angular/core';
 import { User } from '../user.interface';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { UserService } from '../user.service';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -20,13 +20,25 @@ export class UserFormComponent {
 
   private userService = inject(UserService);
   ngOnInit(){
-
+    this.save()
   }
   userForm = new FormGroup({
+    email: new FormControl('',[]),
+    username: new FormControl('',[Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    nombre: new FormControl('', [Validators.required]),
+    lastname: new FormControl('', [Validators.required]),
+    city: new FormControl('', []),
+    street: new FormControl('',[]),
+    number: new FormControl('', []),
+    zipcode: new FormControl('', []),
+    geoLat: new FormControl('', []),
+    geoLong: new FormControl('', []),
 
   })
-  save(){
 
+  save(){
+    console.log(this.userForm)
   }
   close(){
 
